b4bea1f2cfe5c16265da1e8d45bd94bcc601ad06
alan medina <alan.medina@derevo.com.mx>
2024-09-16 20:50:21 -0600

    rename ProfileMetrics component

--- app/_shared/components/user/ProfileMetrics/ProfileMetrics.tsx
+++ app/_shared/components/user/ProfileMetrics/ProfileMetrics.tsx
@@ -1,9 +1,11 @@
 export const ProfileMetrics = () => {
   return (
     <section className="w-full flex items-center justify-center gap-2">
-      <ProfileMetric label="40" metric="Posts" showBorder={true} />
-      <ProfileMetric label="2.8k" metric="Followers" showBorder={true} />
-      <ProfileMetric label="526" metric="Following" showBorder={false} />
+      <ProfileMetric label="40" metric="Posts" />{' '}
+      <span className="text-gray-300">|</span>
+      <ProfileMetric label="2.8k" metric="Followers" />
+      <span className="text-gray-300">|</span>
+      <ProfileMetric label="526" metric="Following" />
     </section>
   );
 };
@@ -11,15 +13,13 @@ export const ProfileMetrics = () => {
 const ProfileMetric = ({
   label,
   metric,
-  showBorder,
 }: {
   label: string;
   metric: string;
-  showBorder: boolean;
 }) => {
   return (
     <article
-      className={`flex flex-col items-center p-2 text-center min-w-16 ${showBorder && 'border-r border-greyAccent'} `}
+      className={`flex flex-col items-center p-2 text-center min-w-16  `}
     >
       <span className="text-black">{label}</span>
       <span className="text-gray-500 text-xs">{metric}</span>
