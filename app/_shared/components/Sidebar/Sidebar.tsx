2ab143531ee72792ada65d5f3a8da1726cf03cb1
alan medina <alan.medina@derevo.com.mx>
2024-09-16 20:50:35 -0600

    make sticky sidebar

--- app/_shared/components/Sidebar/Sidebar.tsx
+++ app/_shared/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,8 @@
-import { ProfileCard } from '@/components/user/UserProfileCard/UserProfile';
+import { ProfileCard } from '@/app/_shared/components/User/UserProfile';
 import { NavMenu } from './NavMenu';
 export const Sidebar = () => {
   return (
-    <aside className="w-60 py-4 px-2 flex flex-col items-center h-full bg-greyBase border gap-4 ">
+    <aside className="w-60 py-4 px-2 flex flex-col items-center h-full bg-greyBase border gap-4 sticky top-0">
       <h2 className="text-black font-bold ">Couch Surfing Social</h2>
 
       <div className="w-full flex flex-col p-2 gap-4">
